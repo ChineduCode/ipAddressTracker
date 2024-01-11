c9483fab7e0b6615f3233b3aee7e86469da1a879
chineducode <chinedunnaemeka2@gmail.com>
2024-01-11 09:56:30 +0100

    modified:   app.js         deleted:    routes/users.js

--- app.js
+++ app.js
@@ -5,12 +5,7 @@ const path = require('path')
 const app = express()
 app.use(express.urlencoded({extended: false}))
 
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'pug')
-
-
 app.use('/', require('./routes/index'))
-app.use('/users', require('./routes/users'))
 
 const PORT = process.env.PORT || 8000
 app.listen(PORT, ()=> console.log(`Server running on port, ${PORT}`))
