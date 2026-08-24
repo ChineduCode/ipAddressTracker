70f065fea5f845362abd8993dfd9a581256522fa
ChineduCode <chinedunnaemeka2@gmail.com>
2026-08-24 13:43:27 +0100

    app.ts updated

--- app.ts
+++ app.ts
@@ -1,8 +1,12 @@
 import express, { Application } from "express";
+import { fileURLToPath } from "url";
 import path from "path";
 import indexRoute from "./routes/index.js";
 
 const app: Application = express();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 //view engine
 app.set('view engine', 'ejs')
