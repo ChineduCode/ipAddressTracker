b79e5ea41ed52af5b91dde5468fda1a730d2201d
ChineduCode <chinedunnaemeka2@gmail.com>
2026-08-24 13:39:29 +0100

    app.ts updated

--- app.ts
+++ app.ts
@@ -1,7 +1,8 @@
-const express = require('express')
-const path = require('path')
+import express from "express";
+import path from "path";
+import indexRoute from "./routes/index.js";
 
-const app = express()
+const app = express();
 
 //view engine
 app.set('view engine', 'ejs')
@@ -12,7 +13,7 @@ app.use(express.urlencoded({extended: false}))
 //css
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.use('/', require('./routes/index'))
+app.use('/', indexRoute);
 
 const PORT = process.env.PORT || 8000
 app.listen(PORT, ()=> console.log(`Server running on port, ${PORT}`))
